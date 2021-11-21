947ae2e3f95a8270996b14522563e59915bd8e36
Gustavo Cornejo <g.cornejovivado@gmail.com>
2021-11-20 21:38:30 -0300

    Add endpoint for wordcount call

--- app.py
+++ app.py
@@ -2,5 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/wordcount', methods=['POST'])
+def wordcountRoute():
+	return 'recived'
+
 if __name__ == '__main__':
 	app.run(debug=True, port=5000)
