6ef799701127c92b6380fc0bf1c159b055e21327
Gustavo Cornejo <g.cornejovivado@gmail.com>
2021-11-21 02:31:36 -0300

    Adapt code to use WordCount class

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 from flask import Flask, request, jsonify
-from wordcount import wordcount
+from classes.wordcount import WordCount
 
 app = Flask(__name__)
 
 @app.route('/wordcount', methods=['POST'])
 def wordcountRoute():
-	text = request.files['file'].read().decode("utf-8")
-	counts = wordcount(text)
-	print(counts)
-	return jsonify(counts)
+	wordcount = WordCount(request.files['file'].read().decode("utf-8"))
+	return jsonify(wordcount())
 
 if __name__ == '__main__':
 	app.run(debug=True, port=5000)
