c71371eb6e843e8b2891ffec08d425edd759836b
Gustavo Cornejo <g.cornejovivado@gmail.com>
2021-11-20 23:27:03 -0300

    Add code to return wordcount to client

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from wordcount import wordcount
 
 app = Flask(__name__)
@@ -8,7 +8,7 @@ def wordcountRoute():
 	text = request.files['file'].read().decode("utf-8")
 	counts = wordcount(text)
 	print(counts)
-	return 'recived'
+	return jsonify(counts)
 
 if __name__ == '__main__':
 	app.run(debug=True, port=5000)
