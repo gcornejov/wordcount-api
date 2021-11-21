0f45e643b36d0fc68ddafe4baba58e750088ca4d
Gustavo Cornejo <g.cornejovivado@gmail.com>
2021-11-20 22:29:27 -0300

    Add code for reading file content from request

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ app = Flask(__name__)
 
 @app.route('/wordcount', methods=['POST'])
 def wordcountRoute():
+	text = request.files['file'].read().decode("utf-8")
+	print(text)
 	return 'recived'
 
 if __name__ == '__main__':
